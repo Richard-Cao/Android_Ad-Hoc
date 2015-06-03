9193d7e85461ac9cd247b90ce5be4ae4ef860749
RichardCao <403164405@qq.com>
2015-06-03 23:09:59 +0800

    增加外部配置发起组网的ssid

--- adhoclibrary/src/main/java/com/googlecode/android/wifi/tether/AdhocControl.java
+++ adhoclibrary/src/main/java/com/googlecode/android/wifi/tether/AdhocControl.java
@@ -8,15 +8,16 @@ import android.content.Intent;
  */
 public class AdhocControl {
 
-    public synchronized static void start(Context context){
+    public synchronized static void start(Context context, TetherApplication application, String ssid) {
+        application.preferenceEditor.putString("ssidpref", ssid).apply();
         // Sending intent to TetherServiceReceiver that we want to start the service-now
         Intent intent = new Intent(TetherService.SERVICEMANAGE_INTENT);
         intent.setAction(TetherService.SERVICEMANAGE_INTENT);
         intent.putExtra("state", TetherService.SERVICE_START);
         context.sendBroadcast(intent);
     }
-    
-    public synchronized static void stop(Context context){
+
+    public synchronized static void stop(Context context) {
         // Sending intent to TetherServiceReceiver that we want to stop the service-now
         Intent intent = new Intent(TetherService.SERVICEMANAGE_INTENT);
         intent.setAction(TetherService.SERVICEMANAGE_INTENT);
--- app/src/main/java/com/njupt/adhoc/MainActivity.java
+++ app/src/main/java/com/njupt/adhoc/MainActivity.java
@@ -1,25 +1,28 @@
 package com.njupt.adhoc;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.googlecode.android.wifi.tether.AdhocControl;
+import com.googlecode.android.wifi.tether.TetherApplication;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     public static final String TAG = "TETHER -> MainActivity";
+    private TetherApplication application = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.application = (TetherApplication) this.getApplication();
         final Button startAdhoc = (Button) findViewById(R.id.start_adhoc);
         final Button endAdhoc = (Button) findViewById(R.id.end_adhoc);
         startAdhoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AdhocControl.start(getApplicationContext());
+                AdhocControl.start(getApplicationContext(), application, "sos");
                 startAdhoc.setClickable(false);
                 endAdhoc.setClickable(true);
             }
